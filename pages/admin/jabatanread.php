f2dbadaaec9fdbc398a049467858b76c7b39c274
Muhammad Fiqri Perdana <mfperdana13@gmail.com>
2022-02-12 20:45:04 +0800

    Membuat tampilan dan perintah tambah jabatan

--- pages/admin/jabatanread.php
+++ pages/admin/jabatanread.php
@@ -44,7 +44,7 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Data Jabatan</h3>
-            <a href="?page=lokasicreate" class="btn btn-success btn-sm float-right">
+            <a href="?page=jabatancreate" class="btn btn-success btn-sm float-right">
                 <i class="fa fa-plus-circle"></i> Tambah Data</a>
         </div>
         <div class="card-body">
